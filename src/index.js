cc1367f025375cd54f6a0aafd639a1dd5cca50a5
XuaanNam <nguyenxuannam0709@gmail.com>
2021-11-04 14:25:03 +0700

    tesst

--- src/index.js
+++ src/index.js
@@ -6,30 +6,32 @@ const path = require('path');
 const app = express();
 const port = 3000;
 
-const route = require('./routes')
+const route = require('./routes');
 
     app.use(express.static(path.join(__dirname, 'public')));
-app.use(express.urlencoded({
-  extended: true
-}));
+    app.use(
+    express.urlencoded({
+        extended: true,
+    })
+);
 app.use(express.json());
 
 //HTTP logger
-    app.use(morgan('combined'));
+app.use(morgan('combined'));
 
 //template engine
-    app.engine("hbs", handlebars({    
-    extname: '.hbs'
-        }));
+app.engine(
+    'hbs',
+    handlebars({
+        extname: '.hbs',
+    })
+);
 app.set('view engine', 'hbs');
 app.set('views', path.join(__dirname, 'resources\\views'));
 
 //routes init
 route(app);
 
-
-
-
 app.listen(port, () => {
-  console.log(`Example app listening at http://localhost:${port}`)
+    console.log(`Example app listening at http://localhost:${port}`);
 });
